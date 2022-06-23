05f91f516daac317cfc16eb629db869e8ed8362a
unknown <hazine505@gmail.com>
2022-06-23 22:46:02 +0300

    added tasks 6 sem

---
+++ HW_6/Task004/Program.cs
@@ -0,0 +1,24 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+void PrintArray (int [] array)
+{
+    for (int i=0; i<array.Length-1; i++)
+    {
+        Console.Write(array[i]+", ");
+    }
+    Console.WriteLine(array[array.Length-1]);
+}
+
+int [] PrintFibb (int N)
+{   int [] array = new int [N];
+    if (N==1) {return array;}
+    
+    array[0]=0;
+    array[1]=1;
+    for (int i=2; i<N;i++)
+    {
+        array[i]=array[i-2]+array[i-1];
+    }
+    return array;
+}
+
+PrintArray(PrintFibb(10));
