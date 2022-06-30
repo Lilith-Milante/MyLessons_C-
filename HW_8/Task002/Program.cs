643b7de052c89f0ea4027e85132cf6ac66c2ef48
unknown <hazine505@gmail.com>
2022-07-01 02:38:59 +0300

    added hw8

---
+++ HW_8/Task002/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] GenerateArray(int row, int column, int min, int max)
+{
+    var array = new int[row, column];
+    var rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] RowsToColunms(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = i + 1; j < array.GetLength(1); j++)
+        {
+            int temp = array[i,j];
+            array [i,j] = array [j,i];
+            array [j,i] = temp;
+        }
+    }
+    return array;
+}
+
+int row = Prompt("Количество строк > ");
+int column = Prompt("Количество столбцов > ");
+int min = -10;
+int max = 10;
+
+int[,] array = GenerateArray(row, column, min, max);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(RowsToColunms(array));
