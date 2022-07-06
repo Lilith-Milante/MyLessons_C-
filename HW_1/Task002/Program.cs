1bf8b5c9a5c5ac752e9ec7d0dc012278dfa78da5
unknown <hazine505@gmail.com>
2022-07-07 02:25:07 +0300

    сделали git add -u

--- HW_1/Task002/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿//Программа, которая на вход принимает значение, а на выходе показывает обратное значение
-
-Console.Write("Введите число  ");
-string numstring  = Console.ReadLine();
-double numberStart = double.Parse(numstring);
-double numberBack = 1 / numberStart;
-Console.Write("Вы получили число  " + numberBack);
--- HW_2/Task003/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа
-
-Console.WriteLine("Введите трёхзначное число   ");
-string numstring  = Console.ReadLine();
-int numberStart = int.Parse(numstring);
-int num = numberStart % 10;
-Console.Write($"Последняя цифра числа: {num}");
