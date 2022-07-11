869a17bd70afacdcd6a7f017b280fc80ce5f5d7c
unknown <hazine505@gmail.com>
2022-07-12 00:34:31 +0300

    start task1 sem 10

--- Lesson_10/Task1Sequence/Program.cs
+++ Lesson_10/Task1Sequence/Program.cs
@@ -1,3 +1,20 @@
 ﻿/* Есть монотонная последовательность, каждое число встречается ровно то количество раз, какое значение этого числа: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,... Выведите ряд этой последовательности до N-го значения, желательно использовать рекурсию:
 N = 12 -> "1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5" */
 
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int numberN = Prompt("Введите N");
+
+int[] Sequence = new int [numberN];
+
+void PrintWord(int[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+    {
+        PrintWord(array, );
+    }
+}
